cf995b3950eaf92eee7657a4f786984d123c3d30
Damir <damir@withpulp.com>
2015-07-27 13:50:34 -0400

    updates to package.js - working on accounts template

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'orionjs:bootstrap',
-  summary: 'A simple theme for orion',
-  version: '1.2.1',
-  git: 'https://github.com/orionjs/orion'
+  name: 'dvz:orion-semantic-ui',
+  summary: 'Semantic-UI Orion Admin Theme',
+  version: '0.0.1',
+  git: 'https://github.com/amazingBastard/orion-semantic-ui'
 });
 
 Package.onUse(function(api) {
@@ -15,55 +15,56 @@ Package.onUse(function(api) {
     'iron:layout@1.0.7',
     'aldeed:autoform@5.1.2',
     'aldeed:tabular@1.1.0',
-    'useraccounts:bootstrap@1.8.1'
-    ]);
+    'useraccounts:semantic-ui@1.11.1',
+    'fabienb4:autoform-semantic-ui@0.4.5'
+  ]);
 
   api.imply([
     'orionjs:core',
     'iron:layout',
     'aldeed:autoform',
-    'useraccounts:bootstrap'
-    ]);
+    'useraccounts:semantic-ui'
+  ]);
 
   api.addFiles([
     'init.js',
     'tabular.js'
-    ]);
+  ]);
 
   api.addFiles([
-    'views/layout/layout.html',
-    'views/layout/layout.js',
-    'views/layout/layout.less',
-    'views/sidebar/sidebar.html',
-    'views/sidebar/sidebar.less',
-    'views/accounts/login.html',
-    'views/accounts/register-with-invitation.html',
-    'views/accounts/index.html',
-    'views/accounts/password.html',
-    'views/accounts/profile.html',
-    'views/accounts/profile.js',
-    'views/accounts/accounts.less',
-    'views/accounts/accounts.html',
-    'views/accounts/update.html',
-    'views/accounts/create.html',
-    'views/config/update.html',
-    'views/config/update.js',
-    'views/dictionary/update.html',
-    'views/dictionary/update.js',
-    'views/collections/index.html',
-    'views/collections/index.js',
-    'views/collections/index.less',
-    'views/collections/create.html',
-    'views/collections/create.js',
-    'views/collections/update.html',
-    'views/collections/update.js',
-    'views/collections/delete.html',
-    'views/pages/index.html',
-    'views/pages/create.html',
-    'views/pages/update.html',
-    'views/pages/delete.html',
-    'views/pages/pages.js',
-    ], 'client');
+    'lib/views/layout/layout.html',
+    'lib/views/layout/layout.js',
+    'lib/views/layout/layout.less',
+    'lib/views/sidebar/sidebar.html',
+    'lib/views/sidebar/sidebar.less',
+    'lib/views/accounts/login.html',
+    'lib/views/accounts/register-with-invitation.html',
+    'lib/views/accounts/index.html',
+    'lib/views/accounts/password.html',
+    'lib/views/accounts/profile.html',
+    'lib/views/accounts/profile.js',
+    'lib/views/accounts/accounts.less',
+    'lib/views/accounts/accounts.html',
+    'lib/views/accounts/update.html',
+    'lib/views/accounts/create.html',
+    'lib/views/config/update.html',
+    'lib/views/config/update.js',
+    'lib/views/dictionary/update.html',
+    'lib/views/dictionary/update.js',
+    'lib/views/collections/index.html',
+    'lib/views/collections/index.js',
+    'lib/views/collections/index.less',
+    'lib/views/collections/create.html',
+    'lib/views/collections/create.js',
+    'lib/views/collections/update.html',
+    'lib/views/collections/update.js',
+    'lib/views/collections/delete.html',
+    'lib/views/pages/index.html',
+    'lib/views/pages/create.html',
+    'lib/views/pages/update.html',
+    'lib/views/pages/delete.html',
+    'lib/views/pages/pages.js',
+  ], 'client');
 
   api.export('orion');
 });
