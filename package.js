de2c7da8917a815dd30b939529c0f8eefe86ff85
Damir <damir@withpulp.com>
2015-07-30 16:45:54 -0400

    fixed typo in sidebar.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dvz:orion-semantic-ui',
   summary: 'Semantic-UI Orion Admin Theme',
-  version: '0.0.8',
+  version: '0.0.9',
   git: 'https://github.com/amazingBastard/orion-semantic-ui'
 });
 
