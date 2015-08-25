4dbc47dec9c117c083b97b673418e338f6680541
Damir <damir@withpulp.com>
2015-08-25 14:02:26 -0400

    updated grouped buttons - fixed container grids

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dvz:orion-semantic-ui',
   summary: 'Semantic-UI Orion Admin Theme',
-  version: '0.5.0',
+  version: '0.5.1',
   git: 'https://github.com/amazingBastard/orion-semantic-ui'
 });
 
