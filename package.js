b3ad057841ed5f922557cf4964f52c5398e6f82b
Damir <damir@withpulp.com>
2015-08-25 13:36:14 -0400

    updated layout grids

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dvz:orion-semantic-ui',
   summary: 'Semantic-UI Orion Admin Theme',
-  version: '0.4.7',
+  version: '0.4.8',
   git: 'https://github.com/amazingBastard/orion-semantic-ui'
 });
 
